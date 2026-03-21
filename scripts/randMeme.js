abb3557524e378abfdf6b20c427a83964fdc0af6
SolotheDolphin <138714961+SolotheDolphin@users.noreply.github.com>
2026-03-21 08:55:46 -0500

    Working on random image selection

--- scripts/randMeme.js
+++ scripts/randMeme.js
@@ -1,4 +1,15 @@
-const animal = "cat"; 
-// import { getAnimalType } from "./index.js";
-// animal = getAnimalType();
-//document.getElementById("animal-type").innerHTML = animal;
+moodAnimal = "cat"; 
+const joyMeter= "super sad";
+// // import { getAnimalType } from "./index.js";
+// //import { getJoyMeter } from "./index.js";
+// //joyMeter = getJoyMeter();
+// // animal = getAnimalType();
+// //document.getElementById("animal-type").innerHTML = animal;
+  document.getElementById('folderInput').addEventListener('change', (event) => {
+    const files = event.target.files;
+    const countElement = document.getElementById('fileCount');
+    countElement.textContent = `${files.length} files selected`;
+  });
+
+
+

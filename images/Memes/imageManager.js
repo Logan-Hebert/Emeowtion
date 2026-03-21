b2ead201cd0788274ce2eedcd83bdcff6e4b509e
SolotheDolphin <138714961+SolotheDolphin@users.noreply.github.com>
2026-03-21 09:26:03 -0500

    Update imageManager.js

--- images/Memes/imageManager.js
+++ images/Memes/imageManager.js
@@ -1,8 +1,21 @@
 //not using node.js, so need to put file nums manually since cant directly read files from folders.
-const catMemeNum = 1;
-const dogMemeNum = 0;
-const horseMemeNum = 0;
-const fishMemeNum = 0;
-const pandaMemeNum = 0;
+//THIS IS FOR THE MEMES IMAGES ONLY
+const sadCatNum = 0;
+const sadDogNum = 0;
+const sadHorseNum = 0;
+const sadFishNum = 0;
+const sadPandaNum = 0;
+
+const happyCatNum = 0;
+const happyDogNum = 0;
+const happyHorseNum = 0;
+const happyFishNum = 0;
+const happyPandaNum = 0;
+
+catMemeNum = sadCatNum + happyCatNum;
+dogMemeNum = sadDogNum + happyDogNum;
+horseMemeNum = sadHorseNum + happyHorseNum;
+fishMemeNum = sadFishNum + happyFishNum;
+pandaMemeNum = sadPandaNum + happyPandaNum;
 
 totalMemeNum = catMemeNum + dogMemeNum + horseMemeNum + fishMemeNum + pandaMemeNum;

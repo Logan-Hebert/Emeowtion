35aef2e79e1a4f151c160184eebc02dc933736e7
SolotheDolphin <138714961+SolotheDolphin@users.noreply.github.com>
2026-03-21 20:27:26 -0500

    Added images so we have naming scheme

---
+++ scripts/cookieGrabber.js
@@ -0,0 +1,7 @@
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", function(event) {
+
+    // event.preventDefault(); 
+
+    console.log("Button was clicked!");
+});
